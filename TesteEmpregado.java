bcab7caa98c15e7803b1093435554e2337277147
Gabriel Kenji <gkpo2014@hotmail.com>
2020-09-04 12:20:37 -0300

    Classe TesteEmpregado Concluida!!

--- TesteEmpregado.java
+++ TesteEmpregado.java
@@ -5,7 +5,23 @@ public class TesteEmpregado {
     Empregado e1 = new Empregado();
     Empregado e2 = new Empregado();
     Empregado e3 = new Empregado();
-    // e agora, como configurar o tipo deles?
+    
+    e1.setTipo(1);
+    e2.setTipo(2);
+    e3.setTipo(3);
+
+    e1.setSalario(2000);
+
+    e2.setSalario(1700);
+    e2.setComissao(0.2);
+
+    e3.setSalario(1500);
+    e3.setBonus(400);
+
+    System.out.println(e1.calcula_salario());
+    System.out.println(e2.calcula_salario());
+    System.out.println(e3.calcula_salario());
+
 
    }
 
